b205cb1b6a8919c77ecc3cf2fe6b3cb7ee22d625
Vinit Kumar <vinitkumar8092@gmail.com>
2026-07-07 10:33:02 +0530

    Added Operator in Chapter 2

---
+++ Chapter2/03_operators.py
@@ -0,0 +1,10 @@
+# Arithematic Operators
+a = 10
+b = 5
+c = a + b # Addition
+d = a - b # Subtraction 
+e = a * b # Multiplication
+f = a / b # Division
+g = a % b # Modulus
+h = a ** b # Exponentiation
+print(c, d, e, f, g, h)
